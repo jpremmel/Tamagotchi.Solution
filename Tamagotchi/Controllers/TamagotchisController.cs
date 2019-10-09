2a72fe4c054f68ba817d541817e3c05a9c1be29f
Jacqueline Remmel and Loewy Malkovich <jacqueline@>
2019-10-09 11:42:55 -0700

    try to make buttons functional for other tamagotchis

--- Tamagotchi/Controllers/TamagotchisController.cs
+++ Tamagotchi/Controllers/TamagotchisController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Tamagotchi.Models;
 using System.Collections.Generic;
+using System;
 
 namespace Tamagotchi.Controllers
 {
@@ -14,5 +15,63 @@ namespace Tamagotchi.Controllers
             Pet pet3 = new Pet(name3);
             return View(Pet.PetsList);
         }
+
+        [HttpPost("/tamagotchis/feed/{i}")]
+        public ActionResult ShowFeed(string n)
+        {
+            Console.WriteLine(">>>>>>>>>>>>>>>>> n = " + n);
+            // int i = int.Parse(n);
+            Console.WriteLine(">>>>>>>>>>>>>>>>> i = " + i);
+            for (int j = 0; j < Pet.PetsList.Count; j++)
+            {
+                if (j == i)
+                {
+                    Pet.PetsList[j].Feed();
+                }
+                else
+                {
+                    Pet.PetsList[j].Ignore();
+                }
+            }
+            return View(Pet.PetsList);
+        }
+
+        [HttpPost("/tamagotchis/sleep/{i}")]
+        public ActionResult ShowSleep(int i)
+        {
+            Console.WriteLine(">>>>>>>>>>>>>>>>> i = " + i);
+            for (int j = 0; j < Pet.PetsList.Count; j++)
+            { 
+                if (j == i)
+                {
+                    Pet.PetsList[j].Sleep();
+                }
+                else
+                {
+                    Pet.PetsList[j].Ignore();
+                }
+            }
+            return View(Pet.PetsList);
+        }
+
+        [HttpPost("/tamagotchis/play/{i}")]
+        public ActionResult ShowPlay(int i)
+        {
+            Console.WriteLine(">>>>>>>>>>>>>>>>> i = " + i);
+            for (int j = 0; j < Pet.PetsList.Count; j++)
+            {
+                if (j == i)
+                {
+                    Pet.PetsList[j].Play();
+                }
+                else
+                {
+                    Pet.PetsList[j].Ignore();
+                }
+            }
+            return View(Pet.PetsList);
+        }
+
+        
     }
 }
