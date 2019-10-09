13ddc73015fef5cc4b7373b615d7bbdc1dbd1dc4
Jacqueline Remmel and Loewy Malkovich <jacqueline@>
2019-10-09 09:03:24 -0700

    Add backend Pet class.

---
+++ Tamagotchi/Models/Tamagotchi.cs
@@ -0,0 +1,47 @@
+
+namespace Tamagotchi.Models
+{
+    public class Pet
+    {
+        public string Name { get; }
+        public int FoodLevel { get; set; }
+        public int Mood { get; set; }
+        public int Fatigue { get; set; }
+
+        public Pet(string name)
+        {
+            Name = name;
+            FoodLevel = 10;
+            Mood = 10;
+            Fatigue = 10;
+        }
+
+        public void Feed()
+        {
+            FoodLevel = 10;
+            Mood -= 2;
+            Fatigue -= 2;
+        }
+
+        public void Play()
+        {
+            Mood = 10;
+            FoodLevel -= 2;
+            Fatigue -= 2;
+        }
+
+        public void Sleep()
+        {
+            Fatigue = 10;
+            Mood -= 2;
+            FoodLevel -= 2;
+        }
+
+        public void Ignore()
+        {
+            Fatigue -= 2;
+            Mood -= 2;
+            FoodLevel -= 2;
+        }
+    }
+}
